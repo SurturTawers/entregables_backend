d73529941b86686a7dc7648ed3ff6d5ecde6d78f
SurturTawers <davtorresga@gmail.com>
2023-03-07 22:56:24 -0300

    path fix

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import __dirname from "../utils.js";
 import * as fs from 'fs';
 const viewsRouter = Router();
 
-const path = './public/storage';
+const path = __dirname+'/public/storage';
 viewsRouter.get('/',(req,res)=>{
     const products = JSON.parse(fs.readFileSync(path+'/productos.json'));
     res.render('home',{products:products});
